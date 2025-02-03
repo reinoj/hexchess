4f5c6882368eb74f1435bc380182e2b609c72c95
reinoj <38120663+reinoj@users.noreply.github.com>
2025-02-03 02:33:06 -0600

    knight and bishop movement

--- piece.gd
+++ piece.gd
@@ -86,13 +86,13 @@ func get_pawn_moves(hex: Vector2i):
 	# move 1 forward
 	var move_one: bool = false
 	var test_hex: Vector2i = pawn_next_hex(hex)
-	if test_move_hex(test_hex):
+	if test_pawn_move_hex(test_hex):
 		move_locations.append(HexFunctions.axial_to_oddq(test_hex))
 		move_one = true
 	# move 2 forward if in a start location
 	if move_one and hex in Globals.pawn_starts[piece_team]:
 		test_hex = pawn_next_hex(test_hex)
-		if test_move_hex(test_hex):
+		if test_pawn_move_hex(test_hex):
 			move_locations.append(HexFunctions.axial_to_oddq(test_hex))
 	# side captures
 	if piece_team == PieceTeam.WHITE:
@@ -109,76 +109,224 @@ func pawn_next_hex(hex: Vector2i) -> Vector2i:
 		return Vector2i(hex.x, hex.y + 1)
 
 # axial
-func pawn_side_captures(test_hex: Vector2i):
-	if Globals.locations[PieceEnum.other_team(piece_team)].has(test_hex):
-		move_locations.append(HexFunctions.axial_to_oddq(test_hex))
-	test_hex.x += 2
-	test_hex.y -= 1
-	if Globals.locations[PieceEnum.other_team(piece_team)].has(test_hex):
-		move_locations.append(HexFunctions.axial_to_oddq(test_hex))
+func pawn_side_captures(hex: Vector2i):
+	if Globals.locations[PieceEnum.other_team(piece_team)].has(hex):
+		move_locations.append(HexFunctions.axial_to_oddq(hex))
+	hex.x += 2
+	hex.y -= 1
+	if Globals.locations[PieceEnum.other_team(piece_team)].has(hex):
+		move_locations.append(HexFunctions.axial_to_oddq(hex))
 
 # axial
 func get_rook_moves(hex: Vector2i, longest_move: int):
 	# q-axis
 	var test_hex: Vector2i = Vector2i(hex.x, hex.y+1)
 	for i in range(longest_move):
-		if test_move_hex(test_hex):
+		var test_res: Array[bool] = test_move_hex(test_hex)
+		if test_res[0]:
 			move_locations.append(HexFunctions.axial_to_oddq(test_hex))
 			test_hex.y += 1
+			if test_res[1]:
+				break
 		else:
 			break
 	test_hex = Vector2i(hex.x, hex.y-1)
 	for i in range(longest_move):
-		if test_move_hex(test_hex):
+		var test_res: Array[bool] = test_move_hex(test_hex)
+		if test_res[0]:
 			move_locations.append(HexFunctions.axial_to_oddq(test_hex))
 			test_hex.y -= 1
+			if test_res[1]:
+				break
 		else:
 			break
 	# r-axis
 	test_hex = Vector2i(hex.x+1, hex.y)
 	for i in range(longest_move):
-		if test_move_hex(test_hex):
+		var test_res: Array[bool] = test_move_hex(test_hex)
+		if test_res[0]:
 			move_locations.append(HexFunctions.axial_to_oddq(test_hex))
 			test_hex.x += 1
+			if test_res[1]:
+				break
 		else:
 			break
 	test_hex = Vector2i(hex.x-1, hex.y)
 	for i in range(longest_move):
-		if test_move_hex(test_hex):
+		var test_res: Array[bool] = test_move_hex(test_hex)
+		if test_res[0]:
 			move_locations.append(HexFunctions.axial_to_oddq(test_hex))
 			test_hex.x -= 1
+			if test_res[1]:
+				break
 		else:
 			break
 	# s-axis
 	test_hex = Vector2i(hex.x+1, hex.y-1)
 	for i in range(longest_move):
-		if test_move_hex(test_hex):
+		var test_res: Array[bool] = test_move_hex(test_hex)
+		if test_res[0]:
 			move_locations.append(HexFunctions.axial_to_oddq(test_hex))
 			test_hex.x += 1
 			test_hex.y -= 1
+			if test_res[1]:
+				break
 		else:
 			break
-	# negative s
 	test_hex = Vector2i(hex.x-1, hex.y+1)
 	for i in range(longest_move):
-		if test_move_hex(test_hex):
+		var test_res: Array[bool] = test_move_hex(test_hex)
+		if test_res[0]:
 			move_locations.append(HexFunctions.axial_to_oddq(test_hex))
 			test_hex.x -= 1
 			test_hex.y += 1
+			if test_res[1]:
+				break
 		else:
 			break
 
-# TODO
+# axial
 func get_knight_moves(hex: Vector2i):
-	pass
+	# q+
+	var test_hex: Vector2i = Vector2i(hex.x+3, hex.y-1)
+	if test_move_hex(test_hex)[0]:
+		move_locations.append(HexFunctions.axial_to_oddq(test_hex))
+	test_hex.y -= 1
+	if test_move_hex(test_hex)[0]:
+		move_locations.append(HexFunctions.axial_to_oddq(test_hex))
+	
+	# r-
+	test_hex.x -= 1
+	test_hex.y -= 1
+	if test_move_hex(test_hex)[0]:
+		move_locations.append(HexFunctions.axial_to_oddq(test_hex))
+	test_hex.x -= 1
+	if test_move_hex(test_hex)[0]:
+		move_locations.append(HexFunctions.axial_to_oddq(test_hex))
+	
+	# s+
+	test_hex.x -= 2
+	test_hex.y += 1
+	if test_move_hex(test_hex)[0]:
+		move_locations.append(HexFunctions.axial_to_oddq(test_hex))
+	test_hex.x -= 1
+	test_hex.y += 1
+	if test_move_hex(test_hex)[0]:
+		move_locations.append(HexFunctions.axial_to_oddq(test_hex))
+	
+	# q-
+	test_hex.x -= 1
+	test_hex.y += 2
+	if test_move_hex(test_hex)[0]:
+		move_locations.append(HexFunctions.axial_to_oddq(test_hex))
+	test_hex.y += 1
+	if test_move_hex(test_hex)[0]:
+		move_locations.append(HexFunctions.axial_to_oddq(test_hex))
+	
+	# r+
+	test_hex.x += 1
+	test_hex.y += 1
+	if test_move_hex(test_hex)[0]:
+		move_locations.append(HexFunctions.axial_to_oddq(test_hex))
+	test_hex.x += 1
+	if test_move_hex(test_hex)[0]:
+		move_locations.append(HexFunctions.axial_to_oddq(test_hex))
+	
+	# s-
+	test_hex.x += 2
+	test_hex.y -= 1
+	if test_move_hex(test_hex)[0]:
+		move_locations.append(HexFunctions.axial_to_oddq(test_hex))
+	test_hex.x += 1
+	test_hex.y -= 1
+	if test_move_hex(test_hex)[0]:
+		move_locations.append(HexFunctions.axial_to_oddq(test_hex))
 
-# TODO
+# axial
 func get_bishop_moves(hex: Vector2i, longest_move: int):
-	pass
+	# q+
+	var test_hex: Vector2i = Vector2i(hex.x+2, hex.y-1)
+	for i in range(longest_move):
+		var test_res: Array[bool] = test_move_hex(test_hex)
+		if test_res[0]:
+			move_locations.append(HexFunctions.axial_to_oddq(test_hex))
+			test_hex.x += 2
+			test_hex.y -= 1
+			if test_res[1]:
+				break
+		else:
+			break
+	# q-
+	test_hex = Vector2i(hex.x-2, hex.y+1)
+	for i in range(longest_move):
+		var test_res: Array[bool] = test_move_hex(test_hex)
+		if test_res[0]:
+			move_locations.append(HexFunctions.axial_to_oddq(test_hex))
+			test_hex.x -= 2
+			test_hex.y += 1
+			if test_res[1]:
+				break
+		else:
+			break
+	# r+
+	test_hex = Vector2i(hex.x-1, hex.y+2)
+	for i in range(longest_move):
+		var test_res: Array[bool] = test_move_hex(test_hex)
+		if test_res[0]:
+			move_locations.append(HexFunctions.axial_to_oddq(test_hex))
+			test_hex.x -= 1
+			test_hex.y += 2
+			if test_res[1]:
+				break
+		else:
+			break
+	# r-
+	test_hex = Vector2i(hex.x+1, hex.y-2)
+	for i in range(longest_move):
+		var test_res: Array[bool] = test_move_hex(test_hex)
+		if test_res[0]:
+			move_locations.append(HexFunctions.axial_to_oddq(test_hex))
+			test_hex.x += 1
+			test_hex.y -= 2
+			if test_res[1]:
+				break
+		else:
+			break
+	# s+
+	test_hex = Vector2i(hex.x-1, hex.y-1)
+	for i in range(longest_move):
+		var test_res: Array[bool] = test_move_hex(test_hex)
+		if test_res[0]:
+			move_locations.append(HexFunctions.axial_to_oddq(test_hex))
+			test_hex.x -= 1
+			test_hex.y -= 1
+			if test_res[1]:
+				break
+		else:
+			break
+	# s-
+	test_hex = Vector2i(hex.x+1, hex.y+1)
+	for i in range(longest_move):
+		var test_res: Array[bool] = test_move_hex(test_hex)
+		if test_res[0]:
+			move_locations.append(HexFunctions.axial_to_oddq(test_hex))
+			test_hex.x += 1
+			test_hex.y += 1
+			if test_res[1]:
+				break
+		else:
+			break
 
 # true -> can move here
 # axial
-func test_move_hex(hex: Vector2i) -> bool:
+func test_pawn_move_hex(hex: Vector2i) -> bool:
 	return !Globals.locations[PieceTeam.BLACK].has(hex) and \
 		!Globals.locations[PieceTeam.WHITE].has(hex) and \
 		tile_map.get_cell_tile_data(HexFunctions.axial_to_oddq(hex)) != null
+
+# [true -> can move here, true -> enemy piece at location]
+# axial
+func test_move_hex(hex: Vector2i) -> Array[bool]:
+	return [!Globals.locations[piece_team].has(hex) and \
+		tile_map.get_cell_tile_data(HexFunctions.axial_to_oddq(hex)) != null,
+		Globals.locations[PieceEnum.other_team(piece_team)].has(hex)]
